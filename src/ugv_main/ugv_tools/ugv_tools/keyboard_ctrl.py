d90e4157ac533bd0bff0d20f84103e34f5410522
eriklely <eriklely@hotmail.com>
2026-09-02 02:30:13 +0200

    keyboard_ctrl: port pan-tilt + arrow-key control from UGV_ROS2_RPI
    - Add JointState pan/tilt publisher using this repo's 'joint_commands' topic
      (matches joy_ctrl.py in UGV_ROS)
    - Add arrow-key handling for pan/tilt, 'p' to center camera
    - Keep cmd_vel Twist publisher (queue size 1) as before
    - Fix linear_speed_limit typo usage, use linear.x for both drive modes

--- src/ugv_main/ugv_tools/ugv_tools/keyboard_ctrl.py
+++ src/ugv_main/ugv_tools/ugv_tools/keyboard_ctrl.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import sys, select, termios, tty
-
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import JointState
 
 msg = """
-Control Your Car!
+Control Your Car + Pan-Tilt!
 ---------------------------
 Moving around:
    u    i    o
@@ -20,7 +20,11 @@ e/c : increase/decrease only angular speed by 10%
 t/T : x and y speed switch
 s/S : stop keyboard control
 space key, k : force stop
-anything else : stop smoothly
+
+Pan-Tilt (Arrow keys):
+   ↑ / ↓  : tilt up / down
+   ← / →  : pan left / right
+   p      : center camera
 
 CTRL-C to quit
 """
@@ -58,119 +62,140 @@ speedBindings = {
 }
 
 
-
 class ugv_Keyboard(Node):
-	def __init__(self,name):
-		# Initialize the node
-		super().__init__(name)
-		# Create a publisher to publish the Twist message
-		self.pub = self.create_publisher(Twist,'cmd_vel',1)
-		# Declare parameters for linear and angular speed limits
-		self.declare_parameter("linear_speed_limit",1.0)
-		self.declare_parameter("angular_speed_limit",1.0)
-		# Get the parameter values
-		self.linenar_speed_limit = self.get_parameter("linear_speed_limit").get_parameter_value().double_value
-		self.angular_speed_limit = self.get_parameter("angular_speed_limit").get_parameter_value().double_value
-		# Get the terminal settings
-		self.settings = termios.tcgetattr(sys.stdin)
-	def getKey(self):
-		# Set the terminal to raw mode
-		tty.setraw(sys.stdin.fileno())
-		# Wait for input
-		rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
-		# If there is input, read it
-		if rlist: key = sys.stdin.read(1)
-		# If there is no input, set key to empty string
-		else: key = ''
-		# Set the terminal back to normal mode
-		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
-		# Return the key
-		return key
-	def vels(self, speed, turn):
-		# Return the current speed and turn
-		return "currently:\tspeed %s\tturn %s " % (speed,turn)		
-	
+    def __init__(self, name):
+        super().__init__(name)
+        # Create a publisher to publish the Twist message
+        self.pub = self.create_publisher(Twist, 'cmd_vel', 1)
+        # Pan-tilt publisher (matches topic used by joy_ctrl in this repo)
+        self.pt_pub = self.create_publisher(JointState, 'joint_commands', 10)
+
+        # Declare parameters for linear and angular speed limits
+        self.declare_parameter("linear_speed_limit", 1.0)
+        self.declare_parameter("angular_speed_limit", 1.0)
+
+        self.linear_speed_limit = self.get_parameter("linear_speed_limit").get_parameter_value().double_value
+        self.angular_speed_limit = self.get_parameter("angular_speed_limit").get_parameter_value().double_value
+
+        # Get the terminal settings
+        self.settings = termios.tcgetattr(sys.stdin)
+
+        # Pan-tilt state
+        self.pan = 0.0
+        self.tilt = 0.0
+        self.pt_step = 0.08
+
+    def getKey(self):
+        # Set the terminal to raw mode
+        tty.setraw(sys.stdin.fileno())
+        rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+        if rlist:
+            key = sys.stdin.read(1)
+            # Handle arrow keys (they send 3 bytes)
+            if key == '\x1b':
+                key += sys.stdin.read(2)
+        else:
+            key = ''
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
+        return key
+
+    def vels(self, speed, turn):
+        return "currently:\tspeed %s\tturn %s " % (speed, turn)
+
+    def publish_pt(self):
+        js = JointState()
+        js.name = ['pt_base_link_to_pt_link1', 'pt_link1_to_pt_link2']
+        js.position = [float(self.pan), float(self.tilt)]
+        self.pt_pub.publish(js)
+
+
 def main():
-	# Initialize the ROS 2 Python client library
-	rclpy.init()
-	# Create an instance of the ugv_Keyboard class
-	ugv_keyboard = ugv_Keyboard("keyboard_ctrl")
-	# Set the initial speed and turn
-	xspeed_switch = True
-	(speed, turn) = (0.2, 0.5)
-	# Set the initial position
-	(x, th) = (0, 0)
-	# Set the initial status
-	status = 0
-	# Set the initial stop state
-	stop = False
-	# Set the initial count
-	count = 0
-	# Create a Twist message
-	twist = Twist()
-	try:
-		# Print the message
-		print(msg)
-		# Print the initial speed and turn
-		print(ugv_keyboard.vels(speed, turn))
-		# Loop forever
-		while (1):
-			# Get the key
-			key = ugv_keyboard.getKey()
-			# If the key is t or T, switch the x and y speed
-			if key=="t" or key == "T": xspeed_switch = not xspeed_switch
-			# If the key is s or S, stop the keyboard control
-			elif key == "s" or key == "S":
-				print ("stop keyboard control: {}".format(not stop))
-				stop = not stop
-			# If the key is in the moveBindings dictionary, set the x and y position
-			if key in moveBindings.keys():
-				x = moveBindings[key][0]
-				th = moveBindings[key][1]
-				count = 0	
-			# If the key is in the speedBindings dictionary, set the speed and turn
-			elif key in speedBindings.keys():
-				speed = speed * speedBindings[key][0]
-				turn = turn * speedBindings[key][1]
-				count = 0
-				# If the speed is greater than the linear speed limit, set it to the limit
-				if speed > ugv_keyboard.linenar_speed_limit: 
-					speed = ugv_keyboard.linenar_speed_limit
-					print("Linear speed limit reached!")
-				# If the turn is greater than the angular speed limit, set it to the limit
-				if turn > ugv_keyboard.angular_speed_limit: 
-					turn = ugv_keyboard.angular_speed_limit
-					print("Angular speed limit reached!")
-				# Print the current speed and turn
-				print(ugv_keyboard.vels(speed, turn))
-				# If the status is 14, print the message
-				if (status == 14): print(msg)
-				# Increment the status
-				status = (status + 1) % 15
-			# If the key is space or k, set the x and y position to 0
-			elif key == ' ': (x, th) = (0, 0)
-			# If the key is not in the moveBindings or speedBindings dictionaries, increment the count
-			else:
-				count = count + 1
-				# If the count is greater than 4, set the x and y position to 0
-				if count > 4: (x, th) = (0, 0)
-				# If the key is ctrl-c, break the loop
-				if (key == '\x03'): break
-			# If the xspeed_switch is true, set the x speed
-			if xspeed_switch: twist.linear.x = speed * x
-			# If the xspeed_switch is false, set the y speed
-			else: twist.linear.y = speed * x
-			# Set the turn
-			twist.angular.z = turn * th
-			# If the stop state is false, publish the Twist message
-			if not stop: ugv_keyboard.pub.publish(twist)
-			# If the stop state is true, publish an empty Twist message
-			if stop:ugv_keyboard.pub.publish(Twist())
-	except Exception as e: print(e)
-	finally: ugv_keyboard.pub.publish(Twist())
-	# Set the terminal back to normal mode
-	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, ugv_keyboard.settings)
-	# Destroy the node
-	ugv_keyboard.destroy_node()
-	# Shutdown the ROS 2 Python client library
-	rclpy.shutdown()
+    rclpy.init()
+    ugv_keyboard = ugv_Keyboard("keyboard_ctrl")
+
+    xspeed_switch = True
+    (speed, turn) = (0.2, 0.5)
+    (x, th) = (0, 0)
+    status = 0
+    stop = False
+    count = 0
+    twist = Twist()
+
+    try:
+        print(msg)
+        print(ugv_keyboard.vels(speed, turn))
+
+        while True:
+            key = ugv_keyboard.getKey()
+
+            # ---------- Pan-Tilt (Arrow keys + p) ----------
+            if key == '\x1b[A':          # Up arrow
+                ugv_keyboard.tilt += ugv_keyboard.pt_step
+            elif key == '\x1b[B':        # Down arrow
+                ugv_keyboard.tilt -= ugv_keyboard.pt_step
+            elif key == '\x1b[D':        # Left arrow
+                ugv_keyboard.pan -= ugv_keyboard.pt_step
+            elif key == '\x1b[C':        # Right arrow
+                ugv_keyboard.pan += ugv_keyboard.pt_step
+            elif key == 'p' or key == 'P':
+                ugv_keyboard.pan = 0.0
+                ugv_keyboard.tilt = 0.0
+                print("Camera centered")
+
+            # Clamp values
+            ugv_keyboard.pan = max(-3.0, min(3.0, ugv_keyboard.pan))
+            ugv_keyboard.tilt = max(-0.7, min(1.4, ugv_keyboard.tilt))
+            ugv_keyboard.publish_pt()
+
+            # ---------- Original driving code ----------
+            if key == "t" or key == "T":
+                xspeed_switch = not xspeed_switch
+            elif key == "s" or key == "S":
+                print("stop keyboard control: {}".format(not stop))
+                stop = not stop
+
+            if key in moveBindings.keys():
+                x = moveBindings[key][0]
+                th = moveBindings[key][1]
+                count = 0
+            elif key in speedBindings.keys():
+                speed = speed * speedBindings[key][0]
+                turn = turn * speedBindings[key][1]
+                count = 0
+                if speed > ugv_keyboard.linear_speed_limit:
+                    speed = ugv_keyboard.linear_speed_limit
+                    print("Linear speed limit reached!")
+                if turn > ugv_keyboard.angular_speed_limit:
+                    turn = ugv_keyboard.angular_speed_limit
+                    print("Angular speed limit reached!")
+                print(ugv_keyboard.vels(speed, turn))
+                if status == 14:
+                    print(msg)
+                status = (status + 1) % 15
+            elif key == ' ':
+                (x, th) = (0, 0)
+            else:
+                count = count + 1
+                if count > 4:
+                    (x, th) = (0, 0)
+                if key == '\x03':
+                    break
+
+            if xspeed_switch:
+                twist.linear.x = speed * x
+            else:
+                twist.linear.x = speed * x  # Use linear.x for both modes, y is typically not used for differential drive
+            twist.angular.z = turn * th
+
+            if not stop:
+                ugv_keyboard.pub.publish(twist)
+            else:
+                ugv_keyboard.pub.publish(Twist())
+
+    except Exception as e:
+        print(e)
+    finally:
+        ugv_keyboard.pub.publish(Twist())
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, ugv_keyboard.settings)
+        ugv_keyboard.destroy_node()
+        rclpy.shutdown()
