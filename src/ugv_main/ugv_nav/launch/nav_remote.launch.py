c9ead233a00ee4228ec1505ec07355d92e7738a7
eriklely <eriklely@hotmail.com>
2026-09-03 01:06:07 +0200

    Update nav_remote.launch.py

--- src/ugv_main/ugv_nav/launch/nav_remote.launch.py
+++ src/ugv_main/ugv_nav/launch/nav_remote.launch.py
@@ -10,32 +10,33 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
     ugv_nav = get_package_share_directory('ugv_nav')
     nav2 = get_package_share_directory('nav2_bringup')
-
-    map_yaml = os.path.join(ugv_nav, 'maps', 'map.yaml')
-    params = os.path.join(ugv_nav, 'param', 'amcl_teb.yaml')
-    rviz = os.path.join(ugv_nav, 'rviz', 'view_nav_2d.rviz')
+    rviz_cfg = os.path.join(ugv_nav, 'rviz', 'view_nav_2d.rviz')
 
     return LaunchDescription([
-        DeclareLaunchArgument('map', default_value=map_yaml),
-        DeclareLaunchArgument('params_file', default_value=params),
         DeclareLaunchArgument('use_rviz', default_value='true'),
         DeclareLaunchArgument('use_sim_time', default_value='false'),
+        DeclareLaunchArgument(
+            'map', default_value=os.path.join(ugv_nav, 'maps', 'map.yaml')),
+        DeclareLaunchArgument(
+            'params_file',
+            default_value=os.path.join(ugv_nav, 'param', 'amcl_teb.yaml')),
 
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
-                os.path.join(nav2, 'launch', 'bringup_launch.py')
-            ),
+                os.path.join(nav2, 'launch', 'bringup_launch.py')),
             launch_arguments={
                 'map': LaunchConfiguration('map'),
                 'params_file': LaunchConfiguration('params_file'),
                 'use_sim_time': LaunchConfiguration('use_sim_time'),
+                'slam': 'False',
+                'use_composition': 'False',
+                'autostart': 'true',
             }.items(),
         ),
-
         Node(
             package='rviz2',
             executable='rviz2',
-            arguments=['-d', rviz],
+            arguments=['-d', rviz_cfg],
             condition=IfCondition(LaunchConfiguration('use_rviz')),
         ),
     ])
