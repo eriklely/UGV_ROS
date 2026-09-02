a512d9a7c37532d077368bfa3efb36796d79e1ea
eriklely <eriklely@hotmail.com>
2026-09-02 21:16:03 +0200

    Update robot_pose_publisher_launch.py

--- src/ugv_else/robot_pose_publisher/launch/robot_pose_publisher_launch.py
+++ src/ugv_else/robot_pose_publisher/launch/robot_pose_publisher_launch.py
@@ -1,18 +1,26 @@
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use /clock if true (Gazebo only)',
+        ),
         Node(
-            package="robot_pose_publisher", executable="robot_pose_publisher",
-            name="robot_pose_publisher",
-            output="screen",
+            package='robot_pose_publisher',
+            executable='robot_pose_publisher',
+            name='robot_pose_publisher',
+            output='screen',
             emulate_tty=True,
-            parameters=[
-                {"use_sim_time": True},
-                {"is_stamped": True},
-                {"map_frame": "map"},
-                {"base_frame": "base_link"}
-            ]
-        )
+            parameters=[{
+                'use_sim_time': LaunchConfiguration('use_sim_time'),
+                'is_stamped': True,
+                'map_frame': 'map',
+                'base_frame': 'base_link',
+            }],
+        ),
     ])
