2a7da2ddf5cbc21728e8511196c11576528f9c14
eriklely <eriklely@hotmail.com>
2026-09-03 17:50:07 +0200

    Create build_ugv.sh

---
+++ build_ugv.sh
@@ -0,0 +1,4 @@
+cd /home/ws/ugv_ws
+colcon build --packages-select ugv_bringup ugv_chat_ai ugv_description ugv_gazebo ugv_nav ugv_slam ugv_tools ugv_vision ugv_web_app --symlink-install 
+source install/setup.bash 
+
